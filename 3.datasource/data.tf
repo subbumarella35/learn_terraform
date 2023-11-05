22576e98035a3a64e1363f527ccc6792bdb5b8bb
subbumarella <subbumarella1996@gmail.com>
2023-11-05 16:12:35 +0530

    updated

--- 3.datasource/data.tf
+++ 3.datasource/data.tf
@@ -2,5 +2,5 @@ data "aws_security_group" "example" {
    name = "allow-all"
 }
 output "Security_Details" {
-  value=data.aws_security_group.example.id
+  value="${data.aws_security_group.example.id} and vpc id is ${data.aws_security_group.example.vpc_id}"
 }
