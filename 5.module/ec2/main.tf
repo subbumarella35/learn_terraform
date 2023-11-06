b600c294d57f383cf9b796e497132509f3d32c30
subbumarella <subbumarella1996@gmail.com>
2023-11-06 12:14:47 +0530

    upated

--- 5.module/ec2/main.tf
+++ 5.module/ec2/main.tf
@@ -24,13 +24,13 @@ resource "aws_security_group" "security_group_ex"{
         from_port=22
         to_port=22
         protocol="tcp"
-        cidr_block=["0.0.0.0/0"]
+        cidr_blocks=["0.0.0.0/0"]
     }
-    engress{
+    egress{
         from_port=0
         to_port=0
         protocol="-1"
-        cidr_block=["0.0.0.0/0"]
+        cidr_blocks=["0.0.0.0/0"]
     }
     tags = {
         Name = "example-security-group"
